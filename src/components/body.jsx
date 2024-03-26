40851b2902bfb2cfcc24df3d18e89622341f423f
Raj kavankar <rajkavankar@gmail.com>
2024-03-26 19:33:51 +0530

    Location select added

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,120 +1,159 @@
-import React from 'react'
+import React from "react"
 // import { CCard, CRow, CCol, CCardBody, CCardImage, CCardTitle,CCardText} from '@coreui/react'
 // import duke from "../assets/LearndukeLogo.png"
-import Card from './shared/Card'
-
+import Card from "./shared/Card"
+import { Row, Col, Container } from "react-bootstrap"
+import useLocation from "../Context/LocationContext"
 import "./body.css"
 import person from "../assets/person.jpg"
-import { CiLocationOn } from "react-icons/ci";
-import { PiStudentLight } from "react-icons/pi";
+import LocationSelect from "./shared/LocationSelect"
+import { CiLocationOn } from "react-icons/ci"
+import { PiStudentLight } from "react-icons/pi"
 
-function body() {
+function Body() {
+  const { location } = useLocation()
   return (
     <>
-      <div className='container'>
-         <div className='box'>
-          <h1 className=''>The Best Physics Tutors for CBSE in Balasore</h1>
+      <div className=''>
+        <div className='box'>
+          <h1 className=''>The Best Physics Tutors for CBSE in {location}</h1>
           <button className='join_us'>Join Us</button>
           <button className='Learn_more'> learn more</button>
-         </div>
-         <div className='boxy'>
-          {/* <CiLocationOn /> */}
-           <input type="text" placeholder='location' className='boxy_L' autoComplete='off' />
-           <input type="text" placeholder='subject' className='boxy_s' autoComplete='off' />
-           <input type="text" placeholder='exams' className='boxy_e' autoComplete='off' />
-           <input type="text" placeholder='curriculum' className='boxy_c' autoComplete='off' />     
-         </div>
-         <div className='carding'>
-         <Card>
-          <div className='left_side'>
-            <img className='person' src={person} alt="" />
-          </div>
-          <div className='right_side'>
-           <h2 className='person_name'>Shiv-Maths</h2>
-           <h3 className ="person_title">
-            Passionate and seasoned teacher with 5 years of  experience
-            </h3>
-            <div className='exp_taught'>
-            <h6 className='person_exp'> 5 years of experience</h6>
-            <h6 className='person_taught'>170 taught</h6>
+        </div>
+        <Container>
+          <Row className=' boxy'>
+            {/* <CiLocationOn /> */}
+            <Col>
+              <LocationSelect />
+            </Col>
+            {/* <input
+            type='text'
+            placeholder='location'
+            className='boxy_L'
+            autoComplete='off'
+          /> */}
+            <Col>
+              <input
+                type='text'
+                placeholder='subject'
+                className='boxy_s'
+                autoComplete='off'
+              />
+            </Col>
+            <Col>
+              <input
+                type='text'
+                placeholder='exams'
+                className='boxy_e'
+                autoComplete='off'
+              />
+            </Col>
+            <Col>
+              <input
+                type='text'
+                placeholder='curriculum'
+                className='boxy_c'
+                autoComplete='off'
+              />
+            </Col>
+          </Row>
+        </Container>
+        <div className='carding'>
+          <Card>
+            <div className='left_side'>
+              <img className='person' src={person} alt='' />
             </div>
-            <p className='person_desc'> Sarah-Maths is an experienced and passionate teacher with over 5 years of 
-                classroom experience in English in CBSE, ICSE, and IB curriculums.
-                She has helped young learners between the ages of 4 to 7 years acquire
-                healthy social-behavioral skills
-            </p>
-             <h3 className='person_teaches'>Teaches: </h3>
-             <h3 className='person_curri'>Curriculum:</h3>
-             <h3 className='person_exam'>Exam:</h3>
-             <div className='btn'>
-             <button className='teachers_btn'>contact us</button>
-             <button className='teachers_btnn'>session</button>
-             </div>
-          </div>
-         </Card>
+            <div className='right_side'>
+              <h2 className='person_name'>Shiv-Maths</h2>
+              <h3 className='person_title'>
+                Passionate and seasoned teacher with 5 years of experience
+              </h3>
+              <div className='exp_taught'>
+                <h6 className='person_exp'> 5 years of experience</h6>
+                <h6 className='person_taught'>170 taught</h6>
+              </div>
+              <p className='person_desc'>
+                {" "}
+                Sarah-Maths is an experienced and passionate teacher with over 5
+                years of classroom experience in English in CBSE, ICSE, and IB
+                curriculums. She has helped young learners between the ages of 4
+                to 7 years acquire healthy social-behavioral skills
+              </p>
+              <h3 className='person_teaches'>Teaches: </h3>
+              <h3 className='person_curri'>Curriculum:</h3>
+              <h3 className='person_exam'>Exam:</h3>
+              <div className='btn'>
+                <button className='teachers_btn'>contact us</button>
+                <button className='teachers_btnn'>session</button>
+              </div>
+            </div>
+          </Card>
 
-         <br />
-         <Card>
-         <div className='left_side'>
-            <img className='person' src={person} alt="" />
-          </div>
-          <div className='right_side'>
-           <h2 className='person_name'>Shiv-Maths</h2>
-           <h3 className ="person_title">
-            Passionate and seasoned teacher with 5 years of  experience
-            </h3>
-            <div className='exp_taught'>
-            <h6 className='person_exp'> 5 years of experience</h6>
-            <h6 className='person_taught'>170 taught</h6>
+          <br />
+          <Card>
+            <div className='left_side'>
+              <img className='person' src={person} alt='' />
             </div>
-            <p className='person_desc'> Sarah-Maths is an experienced and passionate teacher with over 5 years of 
-                classroom experience in English in CBSE, ICSE, and IB curriculums.
-                She has helped young learners between the ages of 4 to 7 years acquire
-                healthy social-behavioral skills
-            </p>
-             <h3 className='person_teaches'>Teaches: </h3>
-             <h3 className='person_curri'>Curriculum:</h3>
-             <h3 className='person_exam'>Exam:</h3>
-             <div className='btn'>
-             <button className='teachers_btn'>contact us</button>
-             <button className='teachers_btnn'>session</button>
-             </div>
-          </div>
-         </Card>
-         <br />
-         <Card>
-         <div className='left_side'>
-            <img className='person' src={person} alt="" />
-          </div>
-          <div className='right_side'>
-           <h2 className='person_name'>Shiv-Maths</h2>
-           <h3 className ="person_title">
-            Passionate and seasoned teacher with 5 years of  experience
-            </h3>
-            <div className='exp_taught'>
-            <h6 className='person_exp'>  5 years of experience</h6>
-            <h6 className='person_taught'>170 taught</h6>
+            <div className='right_side'>
+              <h2 className='person_name'>Shiv-Maths</h2>
+              <h3 className='person_title'>
+                Passionate and seasoned teacher with 5 years of experience
+              </h3>
+              <div className='exp_taught'>
+                <h6 className='person_exp'> 5 years of experience</h6>
+                <h6 className='person_taught'>170 taught</h6>
+              </div>
+              <p className='person_desc'>
+                {" "}
+                Sarah-Maths is an experienced and passionate teacher with over 5
+                years of classroom experience in English in CBSE, ICSE, and IB
+                curriculums. She has helped young learners between the ages of 4
+                to 7 years acquire healthy social-behavioral skills
+              </p>
+              <h3 className='person_teaches'>Teaches: </h3>
+              <h3 className='person_curri'>Curriculum:</h3>
+              <h3 className='person_exam'>Exam:</h3>
+              <div className='btn'>
+                <button className='teachers_btn'>contact us</button>
+                <button className='teachers_btnn'>session</button>
+              </div>
             </div>
-            <p className='person_desc'> Sarah-Maths is an experienced and passionate teacher with over 5 years of 
-                classroom experience in English in CBSE, ICSE, and IB curriculums.
-                She has helped young learners between the ages of 4 to 7 years acquire
-                healthy social-behavioral skills
-            </p>
-             <h3 className='person_teaches'>Teaches: </h3>
-             <h3 className='person_curri'>Curriculum:</h3>
-             <h3 className='person_exam'>Exam:</h3>
-             <div className='btn'>
-             <button className='teachers_btn'>contact us</button>
-             <button className='teachers_btnn'>session</button>
-             </div>
-             
-          </div>
-         </Card>
-         <br />
-         </div>
+          </Card>
+          <br />
+          <Card>
+            <div className='left_side'>
+              <img className='person' src={person} alt='' />
+            </div>
+            <div className='right_side'>
+              <h2 className='person_name'>Shiv-Maths</h2>
+              <h3 className='person_title'>
+                Passionate and seasoned teacher with 5 years of experience
+              </h3>
+              <div className='exp_taught'>
+                <h6 className='person_exp'> 5 years of experience</h6>
+                <h6 className='person_taught'>170 taught</h6>
+              </div>
+              <p className='person_desc'>
+                {" "}
+                Sarah-Maths is an experienced and passionate teacher with over 5
+                years of classroom experience in English in CBSE, ICSE, and IB
+                curriculums. She has helped young learners between the ages of 4
+                to 7 years acquire healthy social-behavioral skills
+              </p>
+              <h3 className='person_teaches'>Teaches: </h3>
+              <h3 className='person_curri'>Curriculum:</h3>
+              <h3 className='person_exam'>Exam:</h3>
+              <div className='btn'>
+                <button className='teachers_btn'>contact us</button>
+                <button className='teachers_btnn'>session</button>
+              </div>
+            </div>
+          </Card>
+          <br />
+        </div>
       </div>
     </>
   )
 }
 
-export default body
+export default Body
---
+++ src/components/shared/LocationSelect.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { Form, InputGroup } from "react-bootstrap"
+import { FaMapMarkerAlt } from "react-icons/fa"
+import useLocation from "../../Context/LocationContext"
+
+const LocationSelect = () => {
+  const { setLocation } = useLocation()
+  const locations = [
+    {
+      id: 1,
+      location: "Chennai",
+    },
+    {
+      id: 2,
+      location: "Mumbai",
+    },
+    {
+      id: 3,
+      location: "Surat",
+    },
+  ]
+  return (
+    <div>
+      <Form.Select
+        onChange={(e) => setLocation(e.target.value)}
+        aria-label='Default select example'
+        className='inner-boxy'>
+        <option value='value' className='bg-light'>
+          Select location
+        </option>
+        {locations.map((item) => (
+          <option key={item.id} className='bg-light' value={item.location}>
+            {item.location}
+          </option>
+        ))}
+      </Form.Select>
+    </div>
+  )
+}
+
+export default LocationSelect
