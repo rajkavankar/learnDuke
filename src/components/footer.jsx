778064755e20f714fb62268d310b220f22d0065b
Raj kavankar <rajkavankar@gmail.com>
2024-03-26 22:52:35 +0530

    file renaming

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
+import React from "react"
 import "./footer.css"
-function footer() {
+function Footer() {
   return (
     <div className='footerContainer'>
-       <div className='footer_row'>
-         <div className='footer_col'>
-           <h1>Learn Duke</h1>
-           {/* <p>Lorem ipsum dolor sit amet consectetur,  */}
-            {/* adipnt minus? Quo eum consectetur error libero.</p> */}
-         </div>
-         <div className='footer_col'>
-           <h1 className='col_header'>office</h1>
-           <p className='col_desc'>itpl road</p>
-           <p className='col_desc'>whitefield</p>
-           <p className='col_desc'>karnataka, bangalore</p>
-           <p className='col_desc'>pin 560066</p>
-           <p className='email_id'> learnduke@gmail.com</p>
-         </div>
-         <div className='footer_col'>
-           <h1 className='col_links'>Links</h1>
-           <ul className='column_links'>
-             <li>
-               <a href='#'>Home</a>
-              </li>
-              <li>
-               <a href='#'>Features</a>
-              </li>
-              <li>
-               <a href='#'>About</a>
-              </li>
-              <li>
-               <a href='#'>Services</a>
-              </li>
-           </ul>
-         </div>
-         
-         
-
-       </div>
+      <div className='footer_row'>
+        <div className='footer_col'>
+          <h1>Learn Duke</h1>
+          {/* <p>Lorem ipsum dolor sit amet consectetur,  */}
+          {/* adipnt minus? Quo eum consectetur error libero.</p> */}
+        </div>
+        <div className='footer_col'>
+          <h1 className='col_header'>office</h1>
+          <p className='col_desc'>itpl road</p>
+          <p className='col_desc'>whitefield</p>
+          <p className='col_desc'>karnataka, bangalore</p>
+          <p className='col_desc'>pin 560066</p>
+          <p className='email_id'> learnduke@gmail.com</p>
+        </div>
+        <div className='footer_col'>
+          <h1 className='col_links'>Links</h1>
+          <ul className='column_links'>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>Features</a>
+            </li>
+            <li>
+              <a href='#'>About</a>
+            </li>
+            <li>
+              <a href='#'>Services</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default footer
+export default Footer
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './navbar.css'
-import logo from '../assets/LearndukeLogo.png'
-import { IoSearchOutline } from "react-icons/io5";
-function navbar() {
+import React from "react"
+import "./navbar.css"
+import logo from "../assets/LearndukeLogo.png"
+import { IoSearchOutline } from "react-icons/io5"
+function Navbar() {
   return (
     <div className='navbar'>
-      <img src={logo} alt='' className='logo'/>
+      <img src={logo} alt='' className='logo' />
       <h1 className='logo_title'>Learn Duke</h1>
 
       <div className='search-box'>
-      <IoSearchOutline />
+        <IoSearchOutline />
         <input type='text' placeholder='Search questions' />
-        <img src='' alt='' />    
-             {/*icons for search bar  */}
+        <img src='' alt='' />
+        {/*icons for search bar  */}
       </div>
 
       <ul>
@@ -21,10 +21,8 @@ function navbar() {
         <li>Features</li>
         <li>About</li>
       </ul>
-
-      
     </div>
   )
 }
 
-export default navbar
+export default Navbar
